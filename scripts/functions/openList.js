fe831efd189c9d81ce5e3da32fc9f6a6bf4a0085
fpw2 <fabien.pergaud@gmail.com>
2022-06-09 14:18:57 +0200

    finally no

--- scripts/functions/openList.js
+++ scripts/functions/openList.js
@@ -1,18 +1,16 @@
 export const toggleListResult = () => {
     const $listComboBox = document.querySelectorAll(".combo-box") // mon bloc qui contient bloc input et bloc list-result
     $listComboBox.forEach(comboBox => { // pour chaque bloc
-        console.log("1",comboBox)
         comboBox.addEventListener("click", (e) => { // à chaque clic
             if (e.currentTarget.classList.contains("open")) { // si l'élément ou je clic dans mon bloc à la class open
-                e.currentTarget.classList.remove("open") // je supprime la class open
+                e.currentTarget.classList.remove("open") // je ferme mon bloc list en cliquant sur mon item-list ou mon input
                 e.currentTarget.querySelector("input").placeholder = e.currentTarget.querySelector("input").dataset.type // je remplace mon placeholder
             } else {
-                $listComboBox.forEach(combo => { // 
-                    console.log("2",combo)
-                    combo.classList.remove("open")
+                $listComboBox.forEach(combo => { // je resélectionne tout en bouclant
+                    combo.classList.remove("open") // j'enlève la class open 
                     combo.querySelector("input").placeholder = combo.querySelector("input").dataset.type
                 })
-                e.currentTarget.classList.add("open")
+                e.currentTarget.classList.add("open") // j'ajoute la class open au clic sur le bloc
                 e.currentTarget.querySelector("input").placeholder = "Rechercher un " + e.currentTarget.querySelector("input").dataset.searchType
             }
         })
--- scripts/index.js
+++ scripts/index.js
@@ -141,3 +141,4 @@ displayApparatus(recipes, [])
 displayUtensils(recipes, [])
 toggleListResult()
 eventTag()
+
